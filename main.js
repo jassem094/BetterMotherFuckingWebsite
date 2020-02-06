4edce58c801aef9fa407d49da51cad7bcce98b09
jassem094 <60243218+jassem094@users.noreply.github.com>
2020-02-06 23:09:07 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+          function myFunction() {
+                  var txt;
+                  if (confirm("Hey le site xxxvidsxxx est trop bien. Viens dessus stp please")) {
+                   document.location.href=' https://www.youtube.com/watch?v=dQw4w9WgXcQ';
+                  }
+                                    }
+            function change(){
+                    document.body.style.backgroundColor = "magenta";
+                    var elems = document.getElementsByTagName("p");
+                          for (var i = 0;i < elems.length; i++)
+                          {
+                              elems[i].style.color = "#0000ff";
+                              elems[i].style.fontFamily="courier";}  
+
+
+                    var elems2 = document.getElementsByTagName("h2");
+                          for (var i = 0;i < elems.length; i++)
+                          {
+                              elems2[i].style.color = "#008000";
+                              elems2[i].style.fontFamily="Comic Sans";
+                            }   
+                          }
